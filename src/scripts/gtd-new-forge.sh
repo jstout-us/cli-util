2e52fd5df8dfaf82b22b6cb5a9f7052f91486b47
Justin Stout <jstout-us@jstout.us>
2021-04-07 11:16:51 -0700

    finish new forge

--- src/scripts/gtd-new-forge.sh
+++ src/scripts/gtd-new-forge.sh
@@ -21,6 +21,51 @@ fatal()   { echoerr "[FATAL]   $*" ; exit 1 ; }
 
 make () {
     local TARGET="$HOME/$1"
+
+    mkdir -p "${TARGET}"/"00 - Inbox"/{"10 - Ideas","20 - Sources","30 - Questions"}
+    mkdir -p "${TARGET}"/"10 - Notes"
+    mkdir -p "${TARGET}"/"20 - Mgt & Plans"
+    mkdir -p "${TARGET}"/"30 - Docs"
+    mkdir -p "${TARGET}"/"60 - Children"
+    mkdir -p "${TARGET}"/"90 - Closed Projects"
+    mkdir -p "${TARGET}"/"91 - Lost Projects"
+    mkdir -p "${TARGET}"/"92 - OBE Projects"
+
+cat > "${TARGET}"/"20 - Mgt & Plans"/"00 - README.txt" << EOM
+README
+===================================================================================================
+
+<intro>
+
+Objectives:
+=======================================
+
+1.  TBD
+2.  TBD
+
+
+References:
+=======================================
+
+1.  TBD
+2.  TBD
+
+EOM
+
+cat > "${TARGET}"/"20 - Mgt & Plans"/"01 - Work Plan.txt" << EOM
+Work Plan
+===================================================================================================
+
+*   Release 0.1
+
+*   Release 0.2
+
+*   Release 1.0
+
+*   Release 2.0
+
+EOM
+
 }
 
 if [[ $# -ne 1 ]];
