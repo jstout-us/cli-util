d66acda727540945dabf34db532a83136d0064ea
Justin Stout <jstout-us@jstout.us>
2021-04-07 11:04:56 -0700

    update default layout

--- src/scripts/gtd-new-project.sh
+++ src/scripts/gtd-new-project.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 #
 # Usage:
-#   dev_project_dir.sh make "<path>"
-#   dev_project_dir.sh clean "<path>"
+#   git-new-project.sh PROJECT-NAME
 #
-# Description: Backup and resore $HOME/.config/dev directory; use GPG to protect sensitive data.
-#
-# make: populate a project directory with our standard layout
-# clean: delete all empty directories from a project directory
+# Description: Create a new project directory at $HOME/PROJECT-NAME using standard template.
 #
 # Author: Justin Stout
 # Version: 1.0.0
-
-# Derived from: https://www.nicksantamaria.net/post/boilerplate-bash-script/
 
 set -e
 set -o pipefail
@@ -26,26 +20,67 @@ error()   { echoerr "[ERROR]   $*" ; }
 fatal()   { echoerr "[FATAL]   $*" ; exit 1 ; }
 
 make () {
-    local path="$1"
-    mkdir -p "${path}"/"00 - Notes"/${USER}/{"Cheat Sheets","Journal","Lessons Learned","Notebook"}
-    mkdir -p "${path}"/"20 - Documentation"/{"App Notes","Data Sheets","Design","User"}
-    mkdir -p "${path}"/"40 - Working"/"MS01 - Research"/"T01 - TBD"
-    mkdir -p "${path}"/"99 - Close Out"
+    local TARGET="$HOME/$1"
+
+    mkdir -p "${TARGET}"/"00 - Inbox"
+    mkdir -p "${TARGET}"/"10 - Notes"
+    mkdir -p "${TARGET}"/"20 - Mgt & Plans"
+    mkdir -p "${TARGET}"/"30 - Docs"
+    mkdir -p "${TARGET}"/"40 - Working"/{"MS01 - Bootstrap","MS02 - TBD","MS99 - Close"}
+    mkdir -p "${TARGET}"/"40 - Working"/"MS90 - Close Out"/{"10 - Lessons Learned","20 - Future Work"}
+    mkdir -p "${TARGET}"/"40 - Working"/"MS91 - Publish"/{"T10 - Refs","T20 - Outline","T30 - Draft 1"}
+    mkdir -p "${TARGET}"/"40 - Working"/"MS91 - Publish"/{"T31 - Draft 2","T90 - Finalize","T99 - Publish"}
+    mkdir -p "${TARGET}"/"99 - Closed"
+
+cat > "${TARGET}"/"20 - Mgt & Plans"/"00 - README.txt" << EOM
+README
+===================================================================================================
+
+<intro>
+
+Objectives:
+=======================================
+
+1.  TBD
+2.  TBD
+
+
+References:
+=======================================
+
+1.  TBD
+2.  TBD
+
+EOM
+
+cat > "${TARGET}"/"20 - Mgt & Plans"/"01 - Work Plan.txt" << EOM
+Work Plan
+===================================================================================================
+
+*   MS01 - Bootstrap Project
+
+*   MS02 - TBD
+
+*   MS90 - Close Out
+
+*   MS91 - Publish
+
+*   MS99 - Close Project
+
+EOM
+
+cat > "${TARGET}/40 - Working/MS99 - Close/00 - Conclusions.txt" << EOM
+Conclusions
+===================================================================================================
+
+*   TBD
+
+EOM
 }
 
-clean () {
-    local path="$1"
-    find ${path} -type d -empty -delete
-}
-
-if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
-
-  if [[ $1 == "make" ]]; then
-    make $2
-
-  elif [[ $1 == "clean" ]]; then
-    clean $2
-
-  fi
-
+if [[ $# -ne 1 ]];
+then
+    echo "Missing PROJECT-NAME"
+else
+    make $1
 fi
