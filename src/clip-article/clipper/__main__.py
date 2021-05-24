52381f40f99ecd59373d9ca365c6977fd0e26733
Justin Stout <jstout-us@jstout.us>
2021-05-24 16:53:02 -0700

    fix linter errors

--- src/clip-article/clipper/__main__.py
+++ src/clip-article/clipper/__main__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-
+"""Clipper application entry point."""
 from . import article
 from . import util
 
 
 def main(app):
-
+    """Application entry point."""
     html_raw, html_clean, meta_clipper = article.fetch(app['args']['url'])
     meta_goose, meta_newspaper = article.parse(app['args']['url'], html_raw)
 
     names = ('html_raw.html', 'meta_clipper.json', 'meta_goose.json', 'meta_newspaper.json')
     values = (html_raw, meta_clipper, meta_goose, meta_newspaper)
 
-    attachments=[util.write_attach(app['work_dir'], name, value) for (name, value) in
-        zip(names, values)]
+    attachments = [util.write_attach(app['work_dir'], name, value) for (name, value) in
+                   zip(names, values)]
 
     title_block = app['work_dir'] / 'title_block.md'
 
@@ -23,7 +23,7 @@ def main(app):
     article.clip(
         html=html_clean,
         header=util.get_resource_path('article-header.yaml'),
-        before_body = title_block,
+        before_body=title_block,
         attachments=attachments,
         file_name=app['args']['file_name']
         )
